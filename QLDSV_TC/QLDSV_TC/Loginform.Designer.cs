f3c4b24ee9c1d29318aaf79d5cb6e80125709926
Hello world <nguoitoiyeu7519@gmail.com>
2022-05-24 16:19:50 +0700

    NEW

--- QLDSV_TC/QLDSV_TC/Loginform.Designer.cs
+++ QLDSV_TC/QLDSV_TC/Loginform.Designer.cs
@@ -29,37 +29,210 @@ namespace QLDSV_TC
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label tENPMLabel;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loginform));
+            this.qLDSV_TC_HOME = new QLDSV_TC.QLDSV_TC_HOME();
+            this.v_DSPHANMANHBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.v_DSPHANMANHTableAdapter1 = new QLDSV_TC.QLDSV_TC_HOMETableAdapters.v_DSPHANMANHTableAdapter();
+            this.tableAdapterManager1 = new QLDSV_TC.QLDSV_TC_HOMETableAdapters.TableAdapterManager();
+            this.tENPMComboBoxPHANMANH = new System.Windows.Forms.ComboBox();
+            this.textBoxToUsername = new System.Windows.Forms.TextBox();
+            this.textBoxToPass = new System.Windows.Forms.TextBox();
+            this.buttonToLogin = new System.Windows.Forms.Button();
+            this.buttonToExit = new System.Windows.Forms.Button();
+            this.vDSLTCBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.v_DSLTCTableAdapter = new QLDSV_TC.QLDSV_TC_HOMETableAdapters.v_DSLTCTableAdapter();
+            this.vDSLTCBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            tENPMLabel = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.qLDSV_TC_HOME)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.v_DSPHANMANHBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vDSLTCBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vDSLTCBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // qLDSV_TC_HOME
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 92);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username";
+            this.qLDSV_TC_HOME.DataSetName = "QLDSV_TC_HOME";
+            this.qLDSV_TC_HOME.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // label2
+            // v_DSPHANMANHBindingSource1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(129, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Password";
+            this.v_DSPHANMANHBindingSource1.DataMember = "v_DSPHANMANH";
+            this.v_DSPHANMANHBindingSource1.DataSource = this.qLDSV_TC_HOME;
+            // 
+            // v_DSPHANMANHTableAdapter1
+            // 
+            this.v_DSPHANMANHTableAdapter1.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.Connection = null;
+            this.tableAdapterManager1.CT_DONGHOCPHITableAdapter = null;
+            this.tableAdapterManager1.DANGKYTableAdapter = null;
+            this.tableAdapterManager1.GIANGVIENTableAdapter = null;
+            this.tableAdapterManager1.HOCPHITableAdapter = null;
+            this.tableAdapterManager1.KHOATableAdapter = null;
+            this.tableAdapterManager1.LOPTableAdapter = null;
+            this.tableAdapterManager1.LOPTINCHITableAdapter = null;
+            this.tableAdapterManager1.MONHOCTableAdapter = null;
+            this.tableAdapterManager1.SINHVIENTableAdapter = null;
+            this.tableAdapterManager1.UpdateOrder = QLDSV_TC.QLDSV_TC_HOMETableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // tENPMLabel
+            // 
+            tENPMLabel.AutoSize = true;
+            tENPMLabel.Font = new System.Drawing.Font("Tahoma", 9.25F);
+            tENPMLabel.ForeColor = System.Drawing.Color.Navy;
+            tENPMLabel.Location = new System.Drawing.Point(91, 52);
+            tENPMLabel.Name = "tENPMLabel";
+            tENPMLabel.Size = new System.Drawing.Size(82, 16);
+            tENPMLabel.TabIndex = 1;
+            tENPMLabel.Text = "PHÂN QUYỀN";
+            // 
+            // tENPMComboBoxPHANMANH
+            // 
+            this.tENPMComboBoxPHANMANH.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.v_DSPHANMANHBindingSource1, "TENPM", true));
+            this.tENPMComboBoxPHANMANH.FormattingEnabled = true;
+            this.tENPMComboBoxPHANMANH.Items.AddRange(new object[] {
+            "CÔNG NGHỆ THÔNG TIN",
+            "VIỄN THÔNG ",
+            "PHÒNG KẾ TOÁN"});
+            this.tENPMComboBoxPHANMANH.Location = new System.Drawing.Point(186, 49);
+            this.tENPMComboBoxPHANMANH.Name = "tENPMComboBoxPHANMANH";
+            this.tENPMComboBoxPHANMANH.Size = new System.Drawing.Size(121, 21);
+            this.tENPMComboBoxPHANMANH.TabIndex = 2;
+            this.tENPMComboBoxPHANMANH.SelectedIndexChanged += new System.EventHandler(this.tENPMComboBoxPHANMANH_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Tahoma", 9.25F);
+            label3.ForeColor = System.Drawing.Color.Navy;
+            label3.Location = new System.Drawing.Point(91, 93);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(72, 16);
+            label3.TabIndex = 1;
+            label3.Text = "USERNAME";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Tahoma", 9.25F);
+            label4.ForeColor = System.Drawing.Color.Navy;
+            label4.Location = new System.Drawing.Point(91, 132);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(76, 16);
+            label4.TabIndex = 1;
+            label4.Text = "PASSWORD";
+            // 
+            // textBoxToUsername
+            // 
+            this.textBoxToUsername.Location = new System.Drawing.Point(186, 91);
+            this.textBoxToUsername.Name = "textBoxToUsername";
+            this.textBoxToUsername.Size = new System.Drawing.Size(163, 21);
+            this.textBoxToUsername.TabIndex = 3;
+            // 
+            // textBoxToPass
+            // 
+            this.textBoxToPass.Location = new System.Drawing.Point(186, 130);
+            this.textBoxToPass.Name = "textBoxToPass";
+            this.textBoxToPass.PasswordChar = '*';
+            this.textBoxToPass.Size = new System.Drawing.Size(163, 21);
+            this.textBoxToPass.TabIndex = 3;
+            this.textBoxToPass.UseSystemPasswordChar = true;
+            // 
+            // buttonToLogin
+            // 
+            this.buttonToLogin.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonToLogin.ForeColor = System.Drawing.Color.Blue;
+            this.buttonToLogin.Location = new System.Drawing.Point(94, 198);
+            this.buttonToLogin.Name = "buttonToLogin";
+            this.buttonToLogin.Size = new System.Drawing.Size(106, 32);
+            this.buttonToLogin.TabIndex = 4;
+            this.buttonToLogin.Text = "LOGIN";
+            this.buttonToLogin.UseVisualStyleBackColor = true;
+            this.buttonToLogin.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // buttonToExit
+            // 
+            this.buttonToExit.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonToExit.ForeColor = System.Drawing.Color.Red;
+            this.buttonToExit.Location = new System.Drawing.Point(242, 198);
+            this.buttonToExit.Name = "buttonToExit";
+            this.buttonToExit.Size = new System.Drawing.Size(107, 32);
+            this.buttonToExit.TabIndex = 4;
+            this.buttonToExit.Text = "EXIT";
+            this.buttonToExit.UseVisualStyleBackColor = true;
+            this.buttonToExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // vDSLTCBindingSource
+            // 
+            this.vDSLTCBindingSource.DataMember = "v_DSLTC";
+            this.vDSLTCBindingSource.DataSource = this.qLDSV_TC_HOME;
+            // 
+            // v_DSLTCTableAdapter
+            // 
+            this.v_DSLTCTableAdapter.ClearBeforeFill = true;
+            // 
+            // vDSLTCBindingSource1
+            // 
+            this.vDSLTCBindingSource1.DataMember = "v_DSLTC";
+            this.vDSLTCBindingSource1.DataSource = this.qLDSV_TC_HOME;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataMember = "v_DSPHANMANH";
+            this.bindingSource1.DataSource = this.qLDSV_TC_HOME;
+            // 
+            // bindingSource2
+            // 
+            this.bindingSource2.DataMember = "v_DSPHANMANH";
+            this.bindingSource2.DataSource = this.qLDSV_TC_HOME;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(186, 167);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "SINH VIÊN";
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // Loginform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 324);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(444, 255);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.buttonToExit);
+            this.Controls.Add(this.buttonToLogin);
+            this.Controls.Add(this.textBoxToPass);
+            this.Controls.Add(this.textBoxToUsername);
+            this.Controls.Add(label4);
+            this.Controls.Add(label3);
+            this.Controls.Add(tENPMLabel);
+            this.Controls.Add(this.tENPMComboBoxPHANMANH);
+            this.IconOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("Loginform.IconOptions.LargeImage")));
             this.Name = "Loginform";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Loginform_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.qLDSV_TC_HOME)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.v_DSPHANMANHBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vDSLTCBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vDSLTCBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +242,32 @@ namespace QLDSV_TC
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private QLDSV_TCDataSet qLDSV_TCDataSet;
+        private System.Windows.Forms.BindingSource v_DSPHANMANHBindingSource;
+        private QLDSV_TCDataSetTableAdapters.v_DSPHANMANHTableAdapter v_DSPHANMANHTableAdapter;
+        private QLDSV_TCDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.ComboBox tENPMComboBox;
+        private System.Windows.Forms.BindingSource vDSPHANMANHBindingSource;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.RadioButton radioButtonSV;
+        private System.Windows.Forms.Button buttonEXIT;
+        private QLDSV_TC_HOME qLDSV_TC_HOME;
+        private System.Windows.Forms.BindingSource v_DSPHANMANHBindingSource1;
+        private QLDSV_TC_HOMETableAdapters.v_DSPHANMANHTableAdapter v_DSPHANMANHTableAdapter1;
+        private QLDSV_TC_HOMETableAdapters.TableAdapterManager tableAdapterManager1;
+        private System.Windows.Forms.ComboBox tENPMComboBoxPHANMANH;
+        private System.Windows.Forms.TextBox textBoxToUsername;
+        private System.Windows.Forms.TextBox textBoxToPass;
+        private System.Windows.Forms.Button buttonToLogin;
+        private System.Windows.Forms.Button buttonToExit;
+        private System.Windows.Forms.BindingSource vDSLTCBindingSource;
+        private QLDSV_TC_HOMETableAdapters.v_DSLTCTableAdapter v_DSLTCTableAdapter;
+        private System.Windows.Forms.BindingSource vDSLTCBindingSource1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- QLDSV_TC/QLDSV_TC/Loginform.cs
+++ QLDSV_TC/QLDSV_TC/Loginform.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,111 @@ namespace QLDSV_TC
 {
     public partial class Loginform : DevExpress.XtraEditors.XtraForm
     {
+        private SqlConnection conn_publisher = new SqlConnection();
+        private bool isSinhVien = false;
         public Loginform()
         {
             InitializeComponent();
+        }
+
+        private void LayDSPM(String cmd)
+        {
+            DataTable dt = new DataTable();
+            if (conn_publisher.State == ConnectionState.Closed) conn_publisher.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd, conn_publisher);
+            da.Fill(dt);
+            conn_publisher.Close();
+            Program.bds_dspm.DataSource = dt;
+            tENPMComboBox.DataSource = Program.bds_dspm;
+            tENPMComboBox.DisplayMember = "TENPM";
+            tENPMComboBox.ValueMember = "TENSERVER";
+        }
+
+        private int KetNoi_CSDLGOC()
+        {
+            if (conn_publisher != null && conn_publisher.State == ConnectionState.Open)
+                conn_publisher.Close();
+            try
+            {
+                conn_publisher.ConnectionString = Program.connstr_publicsher;
+                conn_publisher.Open();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Lỗi kết nối cơ sở dữ liệu. \nBạn xem lại tên Sever của Publisher, và tên CSDL trong chuỗi kết nối.\n" + e.Message);
+                return 0;
+            }
+        }
+
+        private void Loginform_Load_1(object sender, EventArgs e)
+        {
+            /*// TODO: This line of code loads data into the 'qLDSV_TC_HOME.v_DSLTC' table. You can move, or remove it, as needed.
+            this.v_DSLTCTableAdapter.Fill(this.qLDSV_TC_HOME.v_DSLTC);
+            // TODO: This line of code loads data into the 'qLDSV_TC_HOME.v_DSPHANMANH' table. You can move, or remove it, as needed.
+            this.v_DSPHANMANHTableAdapter1.Fill(this.qLDSV_TC_HOME.v_DSPHANMANH);*/
+
+            if (KetNoi_CSDLGOC() == 0) return;
+            LayDSPM("select * from v_DSPHANMANH");
+
+        }
+
+        /// <summary>
+        /// Button Login
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (isSinhVien == false)
+            {
+                if (textBoxToUsername.Text.Trim() == "" || textBoxToPass.Text.Trim() == "")
+                {
+                    MessageBox.Show("Login name và mật khẩu không được trống", "", MessageBoxButtons.OK);
+                    return;
+                }
+            }
+            else
+            {
+                if (textBoxToUsername.Text.Trim() == "")
+                {
+                    MessageBox.Show("Login name không được trống", "", MessageBoxButtons.OK);
+                    return;
+                }
+            }
+
+            if (isSinhVien == true)
+            {
+                Program.mlogin = "svkn";
+                Program.password = "123";
+                if (Program.KetNoi() == 0) return;
+            }
+            else
+            {
+                Program.mlogin = textBoxToUsername.Text; Program.password = textBoxToPass.Text;
+                if (Program.KetNoi() == 0) return;
+            }
+
+            /// get user and login
+            Program.mlogin = textBoxToUsername.Text;
+            Program.password = textBoxToPass.Text;
+
+            //
+            Program.mChinhanh = tENPMComboBox.SelectedIndex;
+            Program.loginDN = Program.mlogin;
+            Program.passwordDN = Program.password;
+
+        }
+
+        private void tENPMComboBoxPHANMANH_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Program.severname = tENPMComboBox.SelectedValue.ToString();
+            }
+            catch (Exception) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close(); Program.mainForm.Close();
         }
     }
 }
